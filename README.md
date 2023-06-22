995ad394f6d4e78c1b2cf65a40a7f0ab00fdcef7
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2023-06-22 12:47:50 +0100

    Basic BST implemented

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mastersdiss
 A library for creating and managing serialized trees.
+
+### TODO 
+ - Creating a function to test out the offset value, and if its coherent to the logic. I am planning on writing a right most traversal by the end of this weekend.
+ - Serializing from the get-go. Abstracting away the logic in a library and using it to define a a tree directly in a serialized format. I will use the AVL logic to automatically create a bst and also allow users to write trees in normal form the tree.
+ - Use of tries (not sure how much more complexity this will add)
+ - Generating a lot of fake data for different types of trees. I will then proceed to implement logic to handle
+      - red, black tree (i still have to think of a way to assign values for red and black nodes)
+      - B and B+ trees
+      - Segment tress
+
+ - Creating test cases to handle. i will use a combination of hunit, doctest (this is used in gibbon as well) and criterion for benchmarking.
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,70 @@
 module Main where
+-- Importing binary libraries to construct byte arrays
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Binary (putWord8)
+import Data.Binary.Put (runPut, Put)
+import Data.Word (Word8)
+
+-- Traditional Tree function
+data BST a = Leaf a | Node a (BST a) (BST a)
+
+-- Serializing it to a list of word 8
+-- we use ByteString.Lazy due to its speed and.
+serializeBST :: BST Int -> [Word8]
+serializeBST tree = BSL.unpack $ runPut (serializeTree tree)
+
+-- There are two possible values given to serialize tree
+serializeTree :: BST Int -> Put
+
+-- Either we receive a leaf, in which case an imperative do block 
+-- will simply place 0 first followed by value of leaf converted.
+serializeTree (Leaf value) = do
+  putWord8 0 -- Leaf indicator
+  putWord8 (fromIntegral value)
+
+-- The second option is more nuanced. We insert the 1 as the node 
+-- indicator, then the offset value for the right most branch, encoding 
+-- it in word8 as well, followed by the value of the node and finally 
+-- a recursive call to handle left and right branches.
+serializeTree (Node value left right) = do
+  putWord8 1 -- Node indicator
+  putWord8 (fromIntegral (offset right))
+  putWord8 (fromIntegral value)
+  serializeTree left
+  serializeTree right
+
+-- For the offset, as we are arranging the tree in pre order, we are 
+-- using depth first search to return values. When we call offset on 
+-- leaf, it will return 1. When we call a node, it will recursively 
+-- calculate and accumulate the offset according to the depth.
+offset :: BST a -> Int
+offset (Leaf _) = 1
+offset (Node _ left right) = 2 + offset left + offset right
+
+traverseSkipLeft :: [Word8] -> [Int]
+traverseSkipLeft [] = []
+traverseSkipLeft (indicator : rest) =
+  case indicator of
+    0 -> []
+    1 -> traverseSkipLeft (drop offset remainingBytes)
+    where
+      value = fromIntegral (head (drop 2 rest))
+      offset = fromIntegral (head (drop 1 rest))
+      remainingBytes = drop (offset + 2) rest
+
+
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let tree = Node 20
+               (Node 5 (Leaf 2) (Leaf 10))
+               (Node 30 (Leaf 25) (Leaf 40))
+  let serialized = serializeBST tree
+  putStrLn "This is the serialized form a sample bst"
+  print serialized
+  
+  let values = traverseSkipLeft serialized
+  putStrLn "Traversing the serialized BST while skipping left branches:"
+  print values
