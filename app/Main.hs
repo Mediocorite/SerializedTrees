6e318b516ad98db77e8df1d53d53555f4cfe5cee
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2023-07-03 18:37:47 +0100

    Added the traversal function

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,8 @@ import qualified Data.ByteString.Lazy as BSL
 import Data.ByteString.Base16 (encode)
 import Data.Word ( Word8 )
 import Data.Int (Int64)
-import GHC.Event.Windows.FFI (pokeOffsetOverlapped)
+import Data.ByteString.Char8 (unpack)
+import Data.Char (ord)
 
 data Tree a = Leaf a | Node a (Tree a) (Tree a) deriving Show
 
@@ -25,7 +26,7 @@ serializeTree (Leaf value) = BB.word8 0x00 <> BB.word8 value
 serializeTree (Node value left right) =
   let leftSerialized = serializeTree left
       rightSerialized = serializeTree right
-      rightOffset = BB.word8 $ fromIntegral $ lengthOfTree left + 1 -- Points to next after calculating left
+      rightOffset = BB.word8 $ fromIntegral $ lengthOfTree left -- Points to next after calculating left
   in BB.word8 0x01 <> BB.word8 value <> rightOffset <> leftSerialized <> rightSerialized
 
 printByteStringAsHex :: B.ByteString -> IO ()
@@ -34,17 +35,32 @@ printByteStringAsHex bs = putStrLn $ unwords $ chunksOf 2 $ show $ encode bs
 printByteStringAsInt :: B.ByteString -> IO ()
 printByteStringAsInt bs = putStrLn $ unwords $ map show $ B.unpack bs
 
-traverseRight :: B.ByteString -> Int -> [Int]
-traverseRight bs offset = 
-  if offset >= B.length bs then
-    []
-  else 
-    case B.index bs offset of 
-      0x01 -> 
-        let off = fromIntegral (B.index bs offset + 1)
-            val = fromIntegral (B.index bs offset + 2)
-        in ([val] <> traverseRight bs off)
-      0x00 -> [fromIntegral (B.index bs offset + 1)]
+-- traverseRight :: B.ByteString -> Int -> [Int]
+-- traverseRight bs offset = 
+--   if offset >= B.length bs then
+--     []
+--   else 
+--     case B.index bs offset of 
+--       0x01 -> 
+--         let off = fromIntegral (B.index bs offset + 1)
+--             val = fromIntegral (B.index bs offset + 2)
+--         in ([val] <> traverseRight bs off)
+--       0x00 -> [fromIntegral (B.index bs offset + 1)]
+
+traverseRight :: B.ByteString -> Int -> [Word8]
+traverseRight bs offset
+  | offset >= B.length bs = []
+  | otherwise =
+    case B.index bs offset of
+      0x01 ->
+        let val = fromIntegral (B.index bs (offset + 1))
+            off = fromIntegral (B.index bs (offset + 2))
+        in val : traverseRight (B.drop (offset + 3) bs) off
+      0x00 -> [fromIntegral (B.index bs (offset + 1))]
+      _ -> error $ "Invalid byte in serialized tree: " ++ show (bsToIntList bs)
+
+bsToIntList :: B.ByteString -> [Int]
+bsToIntList bs = map ord (unpack bs)
 
 main:: IO()
 main = do
@@ -56,6 +72,8 @@ main = do
     print "This is a sample tree we will attempt to generalize"
     print sampleBST
 
+    -- let sampleLength = BSL.length $ BB.toLazyByteString $ BB.word8 256
+    -- print sampleLength
     putStrLn "\n"
     print "Attempt 1: Serialize the tree"
     let serializedBST = serialize sampleBST
